5cc7614bc6af6701fd078e0fbc1f98245f9266aa
deimoss123 <danielsalksnis@gmail.com>
2022-08-31 18:38:17 +0300

    add scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Pet Dating App</title>
       </Head>
-      <div>
+
+      <div className={styles.Home}>
         <h1>Pet Dating App</h1>
       </div>
     </>
