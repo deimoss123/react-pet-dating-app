a86c6fbd7217ab029a014f3b78ca59e95fd69670
deimoss123 <danielsalksnis@gmail.com>
2022-09-01 19:00:01 +0300

    add basic sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import Header from "../components/landing-components/Header/Header";
 import MainScreen from "../components/landing-components/main-screen/MainScreen";
+import SideBar from "../components/landing-components/SideBar/SideBar";
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <>
       <Head>
         <title>Pawtners | Dating For Pets</title>
       </Head>
-      <MainScreen />
       <div className={styles.Home}>
-        <Header />
+        <Header setSidebarOpen={setSidebarOpen} sidebarOpen={sidebarOpen} />
+        <MainScreen />
         <h1>Pet Dating App</h1>
       </div>
     </>
