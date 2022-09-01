d62f79db5b0cf09155b48962432bc20ef83eb3d8
“AdeleAps” <adelea@inbox.lv>
2022-09-01 14:56:49 +0300

    Add main screen component

---
+++ components/landing-components/main-screen/MainScreen.tsx
@@ -0,0 +1,5 @@
+const MainScreen: React.FC = () => {
+  return <div></div>;
+};
+
+export default MainScreen;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import MainScreen from "../components/landing-components/main-screen/MainScreen";
 import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Pet Dating App</title>
+        <title>Pawtners | Dating For Pets</title>
       </Head>
-
+      <MainScreen />
       <div className={styles.Home}>
         <h1>Pet Dating App</h1>
       </div>
