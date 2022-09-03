3b1a00a5214231b21c67d1d869c79169b7a57077
“AdeleAps” <adelea@inbox.lv>
2022-09-03 21:15:46 +0300

    Start working on validation

--- components/landing-components/SignupForm/SignupForm.tsx
+++ components/landing-components/SignupForm/SignupForm.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
-import { Dispatch, FC, SetStateAction, SyntheticEvent } from "react";
+import { Dispatch, FC, SetStateAction, useState, FormEvent } from "react";
 import styles from "./SignupForm.module.scss";
 
 interface Props {
@@ -8,33 +7,116 @@ interface Props {
 }
 
 const SignupForm: FC<Props> = ({ setModalOpen }) => {
-  function onSubmit(e: SyntheticEvent) {
+  
+  const [enteredInput, setEnteredInput] = useState({
+    emailInput: "",
+    usernameInput: "",
+    passwordInput: "",
+  });
+
+  const emailChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredInput({ ...enteredInput, emailInput: e.target.value });
+  };
+
+  const usernameChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredInput({ ...enteredInput, usernameInput: e.target.value });
+  };
+
+  const passwordChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredInput({ ...enteredInput, passwordInput: e.target.value });
+  };
+
+  const [inputIsValid, setInputIsValid] = useState({
+    emailIsValid: true,
+    usernameIsValid: true,
+    passwordIsValid: true,
+  });
+
+  const checkValidity = () => {
+    if (!(enteredInput.emailInput.includes("@"))) {
+      setInputIsValid({
+        ...inputIsValid,
+        emailIsValid: false,
+      });
+    }
+
+    if (enteredInput.usernameInput.trim() == "") {
+      setInputIsValid({
+        ...inputIsValid,
+        usernameIsValid: false,
+      });
+    }
+
+    if (enteredInput.passwordInput.trim() == "") {
+      setInputIsValid({
+        ...inputIsValid,
+        passwordIsValid: false,
+      });
+    }
+  };
+
+  function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    setModalOpen(false);
+    checkValidity();
+    console.log(inputIsValid);
+    console.log(enteredInput);
+    setEnteredInput({
+      emailInput: "",
+      usernameInput: "",
+      passwordInput: "",
+    });
   }
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   return (
     <form className={styles.SignupForm} onSubmit={onSubmit}>
       <h3>Sign Up</h3>
-      <button className={styles.closeBtn}>
+      <button onClick={closeModal} className={styles.closeBtn}>
         <Image src="/icons/x-mark.svg" layout="fill" alt="close modal button" />
       </button>
       <div className={styles.inputFields}>
         <label htmlFor="formEmail">Email</label>
-        <input className={styles.textInput} type="email" id="formEmail" />
+        <input
+          onChange={emailChangeHandler}
+          className={`${styles["textInput"]} ${
+            !inputIsValid.emailIsValid ? styles["error"] : ""
+          }`}
+          type="email"
+          id="formEmail"
+          value={enteredInput.emailInput}
+        />
         <label htmlFor="formUsername">Username</label>
-        <input className={styles.textInput} type="text" id="formUsername" />
+        <input
+          onChange={usernameChangeHandler}
+          className={`${styles["textInput"]} ${
+            !inputIsValid.usernameIsValid ? styles["error"] : ""
+          }`}
+          type="text"
+          id="formUsername"
+          value={enteredInput.usernameInput}
+        />
         <label htmlFor="formPassword">Password</label>
-        <input className={styles.textInput} type="password" id="formPassword" />
+        <input
+          onChange={passwordChangeHandler}
+          className={`${styles["textInput"]} ${
+            !inputIsValid.passwordIsValid ? styles["error"] : ""
+          }`}
+          type="password"
+          id="formPassword"
+          value={enteredInput.passwordInput}
+        />
         <div className={styles.selectWrapper}>
           <div className={styles.genderWrapper}>
             <h2>I&apos;m a:</h2>
             <div>
-              <input type="radio" id="gender-cat" name="gender" />
+              <input value="cat" type="radio" id="gender-cat" name="gender" />
               <label htmlFor="gender-cat">Cat</label>
             </div>
             <div>
-              <input type="radio" id="gender-dog" name="gender" />
+              <input value="dog" type="radio" id="gender-dog" name="gender" />
               <label htmlFor="gender-dog">Dog</label>
             </div>
           </div>
@@ -50,10 +132,10 @@ const SignupForm: FC<Props> = ({ setModalOpen }) => {
             </div>
           </div>
         </div>
+        <button type="submit" className={styles.signupBtn}>
+          Sign Up
+        </button>
       </div>
-      <Link href="/dating">
-        <a className={styles.signupBtn}>Sign Up</a>
-      </Link>
     </form>
   );
 };
