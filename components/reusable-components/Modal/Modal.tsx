0b58091fdbad851402b843599f0db1129aa56ccc
deimoss123 <danielsalksnis@gmail.com>
2022-09-03 15:11:59 +0300

    add animations to modal

--- components/reusable-components/Modal/Modal.tsx
+++ components/reusable-components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import { Dispatch, FC, ReactNode, SetStateAction } from "react";
 import styles from "./Modal.module.scss";
-
+import { motion } from "framer-motion";
 interface Props {
   children: ReactNode;
   isOpen: boolean;
@@ -8,13 +8,41 @@ interface Props {
 }
 
 const Modal: FC<Props> = ({ children, isOpen, setModalOpen }) => {
-  return !isOpen ? null : (
+  return (
     <>
-      <div
+      <motion.div
         className={styles.ModalOverlay}
         onClick={() => setModalOpen(false)}
-      />
-      <div className={styles.Modal}>{children}</div>
+        transition={{
+          duration: 0.3,
+        }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <motion.div
+          className={styles.Modal}
+          onClick={e => e.stopPropagation()}
+          transition={{
+            duration: 0.5,
+            type: "spring",
+          }}
+          initial={{
+            y: "-100vh",
+            opacity: 0,
+          }}
+          animate={{
+            y: "0",
+            opacity: 1,
+          }}
+          exit={{
+            y: "100vh",
+            opacity: 0,
+          }}
+        >
+          {children}
+        </motion.div>
+      </motion.div>
     </>
   );
 };
