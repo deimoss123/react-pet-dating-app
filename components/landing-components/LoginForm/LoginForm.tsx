628f0db71142b017fe026c0ef3495c8a3545f366
“AdeleAps” <adelea@inbox.lv>
2022-09-04 12:03:44 +0300

    Modify styling for forms, form validation logic now 60% works

--- components/landing-components/LoginForm/LoginForm.tsx
+++ components/landing-components/LoginForm/LoginForm.tsx
@@ -1,4 +1,11 @@
-import { Dispatch, FC, SetStateAction, SyntheticEvent } from "react";
+import {
+  Dispatch,
+  FC,
+  SetStateAction,
+  FormEvent,
+  useState,
+  useEffect,
+} from "react";
 import styles from "./LoginForm.module.scss";
 import stylesSignUp from "../SignupForm/SignupForm.module.scss";
 import Link from "next/link";
@@ -9,31 +16,113 @@ interface Props {
 }
 
 const LoginForm: FC<Props> = ({ setModalOpen }) => {
-  function onSubmit(e: SyntheticEvent) {
+  const [enteredInput, setEnteredInput] = useState({
+    emailInput: "",
+    passwordInput: "",
+  });
+
+  const [inputIsValid, setInputIsValid] = useState({
+    emailIsValid: true,
+    passwordIsValid: false,
+  });
+
+  const [formisValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    if (inputIsValid.emailIsValid && inputIsValid.passwordIsValid) {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
+    }
+  }, [inputIsValid.emailIsValid, inputIsValid.passwordIsValid]);
+
+  const checkEmailValidity = () => {
+    if (!enteredInput.emailInput.includes("@")) {
+      setInputIsValid({
+        ...inputIsValid,
+        emailIsValid: false,
+      });
+    } else {
+      setInputIsValid({
+        ...inputIsValid,
+        emailIsValid: true,
+      });
+    }
+  };
+
+  const checkPasswordValidity = () => {
+    if (!enteredInput.passwordInput.trim()) {
+      setInputIsValid({
+        ...inputIsValid,
+        passwordIsValid: false,
+      });
+    } else {
+      setInputIsValid({
+        ...inputIsValid,
+        passwordIsValid: true,
+      });
+    }
+  };
+
+  function onSubmit(e: FormEvent<HTMLFormElement>) {
+    console.log(formisValid);
     e.preventDefault();
-    setModalOpen(false);
   }
 
+  const emailChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredInput({ ...enteredInput, emailInput: e.target.value });
+  };
+
+  const passwordChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredInput({ ...enteredInput, passwordInput: e.target.value });
+  };
+
+  const buttonRedirect = !formisValid ? (
+    <button type="submit" className={stylesSignUp.signupBtn}>
+      Sign Up
+    </button>
+  ) : (
+    <Link href="/dating">
+      <a className={`${stylesSignUp["signupBtn"]}  ${stylesSignUp["link"]}`}>
+        Sign Up
+      </a>
+    </Link>
+  );
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <form className={stylesSignUp.SignupForm} onSubmit={onSubmit}>
+    <form className={stylesSignUp["SignupForm"]} onSubmit={onSubmit}>
       <h3>Log in</h3>
-      <button className={stylesSignUp.closeBtn}>
+      <button onClick={closeModal} className={stylesSignUp.closeBtn}>
         <Image src="/icons/x-mark.svg" layout="fill" alt="close modal button" />
       </button>
       <div className={stylesSignUp.inputFields}>
         <label htmlFor="formEmail">Email</label>
-        <input className={stylesSignUp.textInput} type="email" id="formEmail" />
+        <input
+          onBlur={checkEmailValidity}
+          onChange={emailChangeHandler}
+          className={`${stylesSignUp["textInput"]} ${
+            !inputIsValid.emailIsValid ? stylesSignUp["error"] : " "
+          }`}
+          type="email"
+          id="formEmail"
+        />
         <label htmlFor="formPassword">Password</label>
         <input
-          className={stylesSignUp.textInput}
+          onChange={passwordChangeHandler}
+          onBlur={checkPasswordValidity}
+          className={`${stylesSignUp["textInput"]} ${
+            !inputIsValid.emailIsValid ? stylesSignUp["error"] : " "
+          }`}
           type="password"
           id="formPassword"
         />
+        <p className={styles.forgotPassword}>Forgot Password?</p>
+        {buttonRedirect}
       </div>
-      <p className={styles.forgotPassword}>Forgot Password</p>
-      <Link href="/dating">
-        <a className={stylesSignUp.signupBtn}>Log in</a>
-      </Link>
     </form>
   );
 };
--- components/landing-components/SignupForm/SignupForm.tsx
+++ components/landing-components/SignupForm/SignupForm.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
-import { Dispatch, FC, SetStateAction, useState, FormEvent } from "react";
+import {
+  Dispatch,
+  FC,
+  SetStateAction,
+  useState,
+  FormEvent,
+  useEffect,
+} from "react";
 import styles from "./SignupForm.module.scss";
+import Link from "next/link";
 
 interface Props {
   setModalOpen: Dispatch<SetStateAction<boolean>>;
@@ -19,10 +27,34 @@ const SignupForm: FC<Props> = ({ setModalOpen }) => {
     passwordIsValid: true,
   });
 
+  const [genderCheckBoxCheck, setGenderCheckBoxCheck] = useState(false);
+  const [interestsCheckBoxCheck, setInterestsCheckBoxCheck] = useState(false);
+
+  const [formisValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    if (
+      inputIsValid.emailIsValid &&
+      inputIsValid.passwordIsValid &&
+      inputIsValid.usernameIsValid &&
+      genderCheckBoxCheck &&
+      interestsCheckBoxCheck
+    ) {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
+    }
+  }, [
+    inputIsValid.emailIsValid,
+    inputIsValid.usernameIsValid,
+    inputIsValid.passwordIsValid,
+    genderCheckBoxCheck,
+    interestsCheckBoxCheck
+  ]);
+
   const emailChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEnteredInput({ ...enteredInput, emailInput: e.target.value });
   };
-
 
   const usernameChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEnteredInput({ ...enteredInput, usernameInput: e.target.value });
@@ -32,51 +64,88 @@ const SignupForm: FC<Props> = ({ setModalOpen }) => {
     setEnteredInput({ ...enteredInput, passwordInput: e.target.value });
   };
 
+  const genderCheckBoxHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setGenderCheckBoxCheck(true);
+    }
+  };
+
+  const interestsCheckBoxHandlerCat = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setInterestsCheckBoxCheck(true);
+    } else {
+      setInterestsCheckBoxCheck(false);
+    }
+  }
+
+  const interestsCheckBoxHandlerDog = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setInterestsCheckBoxCheck(true);
+    } else {
+      setInterestsCheckBoxCheck(false);
+    }
+  }
+
+
   const checkEmailValidity = () => {
-    if (!(enteredInput.emailInput.includes("@"))) {
+    if (!enteredInput.emailInput.includes("@")) {
       setInputIsValid({
         ...inputIsValid,
         emailIsValid: false,
+      });
+    } else {
+      setInputIsValid({
+        ...inputIsValid,
+        emailIsValid: true,
       });
     }
   };
 
   const checkUsernameValidity = () => {
-    if (enteredInput.usernameInput.trim() == "") {
+    if (!enteredInput.usernameInput.trim()) {
       setInputIsValid({
         ...inputIsValid,
         usernameIsValid: false,
+      });
+    } else {
+      setInputIsValid({
+        ...inputIsValid,
+        usernameIsValid: true,
       });
     }
   };
 
   const checkPasswordValidity = () => {
-    if (enteredInput.passwordInput.trim() == "") {
+    if (!enteredInput.passwordInput.trim()) {
       setInputIsValid({
         ...inputIsValid,
         passwordIsValid: false,
       });
+    } else {
+      setInputIsValid({
+        ...inputIsValid,
+        passwordIsValid: true,
+      });
     }
-  }
+  };
 
   function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    checkEmailValidity();
-    checkUsernameValidity();
-    checkPasswordValidity();
-
-    console.log(inputIsValid);
-    console.log(enteredInput);
-    setEnteredInput({
-      emailInput: "",
-      usernameInput: "",
-      passwordInput: "",
-    });
   }
 
   const closeModal = () => {
     setModalOpen(false);
   };
+
+  const buttonRedirect = !formisValid ? (
+    <button type="submit" className={styles.signupBtn}>
+      Sign Up
+    </button>
+  ) : (
+    <Link href="/dating">
+      <a className={`${styles["signupBtn"]} ${styles["link"]}`} >Sign Up</a>
+    </Link>
+  );
 
   return (
     <form className={styles.SignupForm} onSubmit={onSubmit}>
@@ -90,7 +159,7 @@ const SignupForm: FC<Props> = ({ setModalOpen }) => {
           onBlur={checkEmailValidity}
           onChange={emailChangeHandler}
           className={`${styles["textInput"]} ${
-            !inputIsValid.emailIsValid ? styles["error"] : ""
+            !inputIsValid.emailIsValid ? styles["error"] : " "
           }`}
           type="email"
           id="formEmail"
@@ -122,29 +191,47 @@ const SignupForm: FC<Props> = ({ setModalOpen }) => {
           <div className={styles.genderWrapper}>
             <h2>I&apos;m a:</h2>
             <div>
-              <input value="cat" type="radio" id="gender-cat" name="gender" />
+              <input
+                onChange={genderCheckBoxHandler}
+                value="cat"
+                type="radio"
+                id="gender-cat"
+                name="gender"
+              />
               <label htmlFor="gender-cat">Cat</label>
             </div>
             <div>
-              <input value="dog" type="radio" id="gender-dog" name="gender" />
+              <input
+                onChange={genderCheckBoxHandler}
+                value="dog"
+                type="radio"
+                id="gender-dog"
+                name="gender"
+              />
               <label htmlFor="gender-dog">Dog</label>
             </div>
           </div>
           <div className={styles.interestsWrapper}>
             <h2>I&apos;m interested in:</h2>
             <div>
-              <input type="checkbox" id="check-cat" />
+              <input
+                onChange={interestsCheckBoxHandlerCat}
+                type="checkbox"
+                id="check-cat"
+              />
               <label htmlFor="check-cat">Cats</label>
             </div>
             <div>
-              <input type="checkbox" id="check-dog" />
+              <input
+                onChange={interestsCheckBoxHandlerDog}
+                type="checkbox"
+                id="check-dog"
+              />
               <label htmlFor="check-dog">Dogs</label>
             </div>
           </div>
         </div>
-        <button type="submit" className={styles.signupBtn}>
-          Sign Up
-        </button>
+        {buttonRedirect}
       </div>
     </form>
   );
