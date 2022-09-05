2ceec662daa179e68d25ae11059928bf3da22c24
Evija Fokrote <evija.fokrote@gmail.com>
2022-09-05 13:34:49 +0300

    add some more pets to dating

--- pages/dating.tsx
+++ pages/dating.tsx
@@ -22,23 +22,22 @@ const Dating: NextPage = () => {
   const [recommendedPets, setRecommendedPets] = useState([
       {
         id: 1,
-        name: "Luna",
-        age: "1 years",
-        type: "dog",
-        image:
-          "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-        gallery: ["/photos/ami.jpeg", "/photos/harrisons.jpg"],
-        description: "I am a dog, Moooooooooooooooooooooooooooooon",
+        name: "Zemene",
+        age: "7 years",
+        type: "cat",
+        image: "/photos/zemene/zemene.jpg",
+        gallery: ["/photos/zemene/zemene.jpg", "/photos/zemene/zemene-2.jpg", "/photos/zemene/zemene-3.jpg", "/photos/zemene/zemene-4.jpg"],
+        description: "No thoughts, head empty, but pretty enough to make up for it. 💋 Will eat all of your lettuce.",
       },
       {
         id: 2,
-        name: "Garp",
-        age: "3 years",
-        type: "cat",
+        name: "Vinnis",
+        age: "4 years",
+        type: "dog",
         image:
-          "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-        gallery: ["/photos/arcijs.jpg", "/photos/zemene.jpg"],
-        description: "I am a cat, my name is Garp",
+          "/photos/vinnis/vinnis-2.jpg",
+        gallery: ["/photos/vinnis/vinnis-1.jpg", "/photos/vinnis/vinnis-2.jpg"],
+        description: "The fluffiest ball of destruction that will steal your heart and chew all your shoes.",
       },
       {
         id: 3,
@@ -48,8 +47,46 @@ const Dating: NextPage = () => {
         image:
           "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
         gallery: ["/photos/freija.png", "/photos/harrisons.jpg"],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt metus et nunc sodales, ac tristique massa sagittis. Sed at nisi nisi. Vivamus ullamcorper cursus elit, sit amet consequat sapien pellentesque vel. Pellentesque lobortis erat quam. Fusce nec nibh porta lectus rhoncus iaculis. Sed rutrum tortor sed metus maximus, quis luctus augue tempor. Sed urna lacus, tristique eget malesuada in, porttitor eu purus. Vivamus in rutrum sem. Duis finibus odio id eros ullamcorper suscipit. Nunc id vestibulum lacus. In nec sapien augue. Sed varius justo urna, vel scelerisque lacus aliquet eget. Aliquam erat volutpat. In quis cursus massa. Ut eget arcu justo. Donec dui eros, euismod at urna ac, lacinia laoreet ligula. Morbi iaculis lorem dui, posuere viverra mi elementum luctus. Cras scelerisque at nisl eu ultricies. Aliquam in purus metus. Vestibulum varius maximus massa, eu fermentum ipsum malesuada pretium. Donec semper dictum aliquam. Proin a viverra sem, vel dignissim velit. Mauris a mattis nibh. Praesent molestie tortor velit, ut fermentum diam placerat et. Donec elementum viverra velit ac hendrerit.",
+        description: "",
       },
+
+      {
+        id: 4,
+        name: "Dore",
+        age: "6 years",
+        type: "dog",
+        image: "/photos/dore/dore-1.png",
+        gallery: ["/photos/dore/dore-1.png", "/photos/dore/dore-2.png", "/photos/dore/dore-3.png", "/photos/dore/dore-4.png", "/photos/dore/dore-5.png"],
+        description: "I feel my very best when I'm outside. If my owners would let me, I would never come inside."
+      },
+
+      {
+        id: 5,
+        name: "Tesa",
+        age: "5 years",
+        type: "dog",
+        image: "/photos/tesa/tesa-1.png",
+        gallery: ["/photos/tesa/tesa-1.png", "/photos/tesa/tesa-2.png", "/photos/tesa/tesa-3.png", "/photos/tesa/tesa-4.png"],
+        description: "I love my humans and I love helping them with house chores even more. I'm the best of the best and I will prove it!!!"
+      },
+
+      {
+        id: 5,
+        name: "Ella",
+        age: "4 years",
+        type: "dog",
+        image: "/photos/ella/ella-1.png",
+        gallery: ["/photos/ella/ella-1.png", "/photos/ella/ella-2.png", "/photos/ella/ella-3.png", "/photos/ella/ella-4.png", "/photos/ella/ella-5.png"],
+        description: "100% Mischievous, 100% bubbly, 200% playful."
+      },
+
+      {
+        id: 6,
+        name: "Tulle",
+        age: "4 years",
+        type: "dog",
+        image: "",
+      }
     ],
   );
 
